da10fa5e73f02175f2665fe4af232b88a4041121
Roman <100953835+Chezy13@users.noreply.github.com>
2022-08-30 17:55:38 +0300

    "main"

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import Footer from "./AppBlocks/Footer";
 import "./styles/App.css";
 import "./styles/Header.css";
 import "./styles/Banner.css";
+import "./styles/Description.css";
 import "./styles/Footer.css";
+import Description from "./AppBlocks/Description";
 
 function App() {
   return (
     <div className="App">
       <Banner/>
+      <Description/>
       <Footer/>
     </div>
       
---
+++ src/AppBlocks/Description.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Logo1 from "../img/Logo1.png";
+import Logo2 from "../img/Logo2.png";
+import Logo3 from "../img/Logo3.png";
+
+const Description = () => {
+    return (
+        <section className='description'>
+            <div className='wrapper'>
+                <div className='description-content'>
+                    <div className='description-content__block'>
+                        <p className='description-content__block-text'>
+                            "What makes Haus so special is its incredible flavors."
+                        </p>
+                        <img src={Logo1} alt="logo" />
+                    </div>
+                    <div className='description-content__block'>
+                        <p className='description-content__block-text'>
+                            “For the wine or cocktail lover who’s tried it all and wants something new and refreshing.”
+                        </p>
+                        <img src={Logo2} alt="logo" />
+                    </div>
+                    <div className='description-content__block'>
+                        <p className='description-content__block-text'>
+                            “Cleaner, responsibly sourced, and lower in alcohol.”
+                        </p>
+                        <img src={Logo3} alt="logo" />
+                    </div>
+                </div>
+            </div>           
+        </section>
+    );
+};
+
+export default Description;
